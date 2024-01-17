3bbe7aaf3f427296aa071ef452d6f5c3c3daaed9
James Lim <jameslimbj@gmail.com>
2024-01-17 15:04:40 +0800

    feat (post): add post handler and queries

---
+++ database/post_queries.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"time"
+	"wall-server/database/models"
+)
+
+const (
+	selectPostsQuery = "SELECT post_id, user_id, title, body, creation_time FROM posts"
+	insertPostQuery  = "INSERT INTO posts (user_id, title, body) VALUES (?, ?, ?)"
+)
+
+func (d *Database) ReadAllPosts() ([]models.Post, error) {
+	rows, err := d.DB.Query(selectPostsQuery)
+	if err != nil {
+		return nil, HandleError(err)
+	}
+	defer rows.Close()
+
+	var posts []models.Post
+	for rows.Next() {
+		var post models.Post
+		var creationTimeStr string
+		if err := rows.Scan(&post.PostID, &post.UserID, &post.Title, &post.Body, &creationTimeStr); err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+
+		post.CreationTime, err = time.Parse("2006-01-02 15:04:05", creationTimeStr)
+		if err != nil {
+			return nil, HandleError(err)
+		}
+
+		posts = append(posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, HandleError(err)
+	}
+
+	return posts, nil
+}
+
+func (d *Database) CreatePost(post models.Post) (int64, error) {
+	var id int64
+
+	err := d.withTransaction(func(tx *sql.Tx) error {
+		result, err := tx.Exec(insertPostQuery, post.UserID, post.Title, post.Body)
+		if err != nil {
+			return err
+		}
+
+		id, err = result.LastInsertId()
+		return err
+	})
+
+	return id, HandleError(err)
+}
