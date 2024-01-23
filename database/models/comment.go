7125a53b0d07bf62364d2d5b884f517834896025
James Lim <jameslimbj@gmail.com>
2024-01-23 13:56:06 +0800

    feat: format comments with username rather than userID

--- database/models/comment.go
+++ database/models/comment.go
@@ -10,6 +10,18 @@ type Comment struct {
 	UserID         int64     `json:"user_id"`
 	Text           string    `json:"text" binding:"required"`
 	CreationTime   time.Time `json:"creation_time"`
+	ReplyID        int64     `json:"reply_id"`
+	IsEdited       bool      `json:"is_edited"`
+	LastEditedTime time.Time `json:"last_edited_time"`
+}
+
+type CommentFormatted struct {
+	CommentID      int64     `json:"comment_id"`
+	PostID         int64     `json:"post_id"`
+	Username       string    `json:"username"`
+	Text           string    `json:"text"` 
+	CreationTime   time.Time `json:"creation_time"`
+	ReplyID        int64     `json:"reply_id"`
 	IsEdited       bool      `json:"is_edited"`
 	LastEditedTime time.Time `json:"last_edited_time"`
 }
