717cc3815992f5cd900bdafe71440435e18b1cbe
James Lim <jameslimbj@gmail.com>
2024-01-23 13:57:12 +0800

    feat: format comments with username rather than userID (#31)

--- database/comment_queries.go
+++ database/comment_queries.go
@@ -7,28 +7,28 @@ import (
 )
 
 const (
-	selectAllCommentsByPostIDQuery = "SELECT comment_id, user_id, post_id, comment_text, creation_time, is_edited, IFNULL(last_edited_time, 'No Edit Time') as last_edited_time FROM post_comments WHERE post_id = ?"
-	selectCommentByIDQuery         = "SELECT comment_id, user_id, post_id, comment_text, creation_time, is_edited, IFNULL(last_edited_time, 'No Edit Time') as last_edited_time FROM post_comments WHERE comment_id = ?"
+	selectAllCommentsByPostIDQuery = "SELECT c.comment_id, c.post_id, c.comment_text, c.creation_time, c.is_edited, IFNULL(c.last_edited_time, 'No Edit Time') as last_edited_time , c.reply_id, u.username FROM post_comments c INNER JOIN users u ON c.user_id = u.user_id WHERE c.post_id = ?"
+	selectCommentByIDQuery         = "SELECT comment_id, user_id, post_id, comment_text, creation_time, reply_id, is_edited, IFNULL(last_edited_time, 'No Edit Time') as last_edited_time FROM post_comments WHERE comment_id = ?"
 	selectCommentAuthorByIDQuery   = "SELECT user_id FROM post_comments WHERE comment_id = ?"
-	insertCommentQuery             = "INSERT INTO post_comments (post_id, user_id, comment_text) VALUES (?, ?, ?)"
+	insertCommentQuery             = "INSERT INTO post_comments (post_id, user_id, comment_text, reply_id) VALUES (?, ?, ?, ?)"
 	updateCommentQuery             = "UPDATE post_comments SET comment_text = ?, is_edited = TRUE, last_edited_time = CURRENT_TIMESTAMP WHERE comment_id = ?"
 	selectLastEditedTimeQuery      = "SELECT last_edited_time FROM post_comments WHERE comment_id = ?"
 	deleteCommentQuery             = "DELETE FROM post_comments WHERE comment_id = ?"
 )
 
-func (d *Database) ReadAllCommentsByPostID(postID int64) ([]models.Comment, error) {
+func (d *Database) ReadAllCommentsByPostID(postID int64) ([]models.CommentFormatted, error) {
 	rows, err := d.DB.Query(selectAllCommentsByPostIDQuery, postID)
 	if err != nil {
 		return nil, HandleError(err)
 	}
 	defer rows.Close()
 
-	comments := make([]models.Comment, 0)
+	comments := make([]models.CommentFormatted, 0)
 	for rows.Next() {
-		var comment models.Comment
+		var comment models.CommentFormatted
 		var creationTimeStr string
 		var editedTimeStr string
-		if err := rows.Scan(&comment.CommentID, &comment.UserID, &comment.PostID, &comment.Text, &creationTimeStr, &comment.IsEdited, &editedTimeStr); err != nil {
+		if err := rows.Scan(&comment.CommentID, &comment.PostID, &comment.Text, &creationTimeStr, &comment.IsEdited, &editedTimeStr, &comment.ReplyID, &comment.Username); err != nil {
 			return nil, HandleError(err)
 		}
 
@@ -93,7 +93,7 @@ func (d *Database) CreateComment(comment models.Comment) (int64, error) {
 	var id int64
 
 	err := d.withTransaction(func(tx *sql.Tx) error {
-		result, err := tx.Exec(insertCommentQuery, comment.PostID, comment.UserID, comment.Text)
+		result, err := tx.Exec(insertCommentQuery, comment.PostID, comment.UserID, comment.Text, comment.ReplyID)
 		if err != nil {
 			return err
 		}
